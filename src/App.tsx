4f09c25ebbf1953f040accfb2c0a62b2388bffae
Elio Jung <dudwns0921@gmail.com>
2022-11-28 12:24:44 +0900

    feat: css reset 추가
    #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`
+
 function App() {
     return (
         <div className="App">
+            <GlobalStyle />
             <p>Hello World</p>
         </div>
     )
