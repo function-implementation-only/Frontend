b997a19dc54fa681932eaec8324cf1da32961df0
puki4416 <102564722+puki4416@users.noreply.github.com>
2022-11-29 18:16:22 +0900

    Revert "chore : 스토리북 세팅"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ['airbnb', 'airbnb-typescript', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off'
-  }
-};
+    env: { browser: true, es6: true, node: true },
+    extends: ['airbnb', 'airbnb-typescript', 'plugin:prettier/recommended'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+}
