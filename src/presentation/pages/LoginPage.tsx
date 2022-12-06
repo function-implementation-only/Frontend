c8c72320544566f382f462b7297355b7b958ec78
Elio Jung <dudwns0921@gmail.com>
2022-12-06 17:09:34 +0900

    fix: 기존 코드 복구

--- src/presentation/pages/LoginPage.tsx
+++ src/presentation/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Input from '../../stories/Input'
+import { Input } from '../../stories/Input'
 
 function LoginPage() {
     const [idValue, setIdValue] = useState<string>('')
