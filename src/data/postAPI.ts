4a32631772662e0d9fec7811967d2121b5391144
Elio Jung <dudwns0921@gmail.com>
2022-12-07 09:09:23 +0900

    refactor: interceptor을 활용한 api 구조화

--- src/data/postAPI.ts
+++ src/data/postAPI.ts
@@ -1,15 +1,7 @@
 import axios from 'axios'
+import setInterceptors from './interceptor'
 
-const BASE_URL = import.meta.env.VITE_API_END_POINT
-
-const instance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        // Access_Token:
-        //     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkdWR3bnMwOTIxQGdtYWlsLmNvbSIsImV4cCI6MTY3MDM3NjA2MCwiaWF0IjoxNjcwMjg5NjYwfQ.RZ4TdySOKTGki233aL6BlBxsErS61gLARadtQU9Y8ws',
-        'Content-Type': 'multipart/form-data',
-    },
-})
+const instance = setInterceptors(axios.create())
 
 function postAnnouncement(payload: FormData) {
     return instance.post('/posts', payload)
