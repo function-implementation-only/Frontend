cc1b81b209af59f39c29374d07fc8657b1218005
Elio Jung <dudwns0921@gmail.com>
2022-12-08 16:45:54 +0900

    fix: rebase side effect 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import LoginModal from './presentation/components/modal/LoginModal'
-import SignupModal from './presentation/components/modal/SignupModal'
 import MainPage from './presentation/pages/MainPage'
 import PostDetailPage from './presentation/pages/Post/PostDetailPage'
 import PostPage from './presentation/pages/Post/PostPage'
 import HeaderComponent from './presentation/components/HeaderComponent'
+// import SignupModal from './presentation/components/modal/SignupModal'
+// import LoginModal from './presentation/components/modal/LoginModal'
 
 const queryClient = new QueryClient()
 
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <HeaderComponent />
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/post/create" element={<PostPage />} />
-                    <Route path="/post/update/:id" element={<PostPage />} />
-                    <Route
-                        path="/post/detail/:id"
-                        element={<PostDetailPage />}
-                    />
-                </Routes>
-                <LoginModal />
-                <SignupModal />
-            </BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <BrowserRouter>
+                    <HeaderComponent />
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/post/create" element={<PostPage />} />
+                        <Route path="/post/update/:id" element={<PostPage />} />
+                        <Route
+                            path="/post/detail/:id"
+                            element={<PostDetailPage />}
+                        />
+                    </Routes>
+                    {/* <LoginModal />
+                    <SignupModal /> */}
+                </BrowserRouter>
+                <ReactQueryDevtools
+                    initialIsOpen={false}
+                    position="bottom-right"
+                />
+            </div>
+        </QueryClientProvider>
     )
 }
 
