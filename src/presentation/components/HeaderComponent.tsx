c5c811273b45674b802cddaa17d2bad63bfd4b82
Elio Jung <dudwns0921@gmail.com>
2022-12-13 17:15:24 +0900

    feat: header 퍼블리싱

--- src/presentation/components/HeaderComponent.tsx
+++ src/presentation/components/HeaderComponent.tsx
@@ -3,8 +3,40 @@ import styled from 'styled-components'
 import useModal from '../../hooks/useModal'
 import LoginModal from './modal/LoginModal'
 import SignupModal from './modal/SignupModal'
+import Logo from '../../assets/images/Logo.svg'
 
-const HeaderComponentLayout = styled.div``
+const HeaderComponentLayout = styled.div`
+    width: 1440px;
+    background-color: white;
+    position: fixed;
+    display: flex;
+    height: var(--header-height);
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 24px;
+    top: 0;
+`
+
+const HeaderComponentLogoBox = styled.div``
+
+const HeaderComponentBtnBox = styled.div`
+    display: grid;
+    grid-auto-flow: column;
+    grid-column-gap: 10px;
+`
+
+const HeaderComponentBtn = styled.button<{ default?: boolean }>`
+    width: 100px;
+    height: 45px;
+    border-radius: 100px;
+    border: none;
+    cursor: pointer;
+    background-color: ${(props) =>
+        props.default ? 'white' : ' var(--primary-color)'};
+    color: ${(props) => (props.default ? 'var(--primary-color)' : 'white')};
+    border: ${(props) =>
+        props.default ? 'solid 1px var(--primary-color)' : 'none'};
+`
 
 function HeaderComponent() {
     const { isShowing: isLoginModalOpen, handleShowing: handleLogin } =
@@ -13,18 +45,32 @@ function HeaderComponent() {
         useModal()
     return (
         <HeaderComponentLayout>
-            <Link to="/post">Post</Link>
-            <button type="button" onClick={handleLogin}>
-                LogIn
-            </button>
-
+            <HeaderComponentLogoBox>
+                <Link to="/">
+                    <img src={Logo} alt="logoImg" />
+                </Link>
+            </HeaderComponentLogoBox>
+            <HeaderComponentBtnBox>
+                <HeaderComponentBtn default type="button" onClick={handleLogin}>
+                    로그인
+                </HeaderComponentBtn>
+                <Link to="/post/create">
+                    <HeaderComponentBtn type="button">
+                        글쓰기
+                    </HeaderComponentBtn>
+                </Link>
+                <HeaderComponentBtn
+                    default
+                    type="button"
+                    onClick={handleSignUp}
+                >
+                    회원가입
+                </HeaderComponentBtn>
+            </HeaderComponentBtnBox>
             <LoginModal
                 isShowing={isLoginModalOpen}
                 handleShowing={handleLogin}
             />
-            <button type="button" onClick={handleSignUp}>
-                SignUp
-            </button>
             <SignupModal
                 isShowing={isSignupModalOpen}
                 handleShowing={handleSignUp}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,17 @@ import reset from 'styled-reset'
 const GlobalStyle = createGlobalStyle`
   ${reset}
   /* other styles */
+  :root {
+    --primary-color: #00C73C;
+    --header-height: 80px;
+  }
+  .App {
+    position: relative;
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+    padding-top: var(--header-height);
+  }
 `
 
 export default GlobalStyle
