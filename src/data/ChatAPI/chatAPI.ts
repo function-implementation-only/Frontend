0fe4633003ecef34f91451c64b62c9c35b0348cf
Kyungseo-Park <troublesome.dev@gmail.com>
2022-12-17 10:55:34 +0900

    feat: api 인터페이스 추가

---
+++ src/data/ChatAPI/chatAPI.ts
@@ -0,0 +1,25 @@
+import { AxiosInstance, AxiosResponse } from 'axios'
+import setInterceptors from './interceptor'
+
+export interface ChatAPIInterface {
+    getChatCount: (id: string) => Promise<AxiosResponse>
+    getChatRooms: () => Promise<AxiosResponse>
+}
+
+export default class ChatAPI implements ChatAPIInterface {
+    axiosInstance: AxiosInstance
+
+    constructor(axiosInstance: AxiosInstance) {
+        this.axiosInstance = axiosInstance
+    }
+
+    getChatCount(id?: string) {
+        return setInterceptors(this.axiosInstance).post('/api/chat/count', {
+            id,
+        })
+    }
+
+    getChatRooms() {
+        return setInterceptors(this.axiosInstance).post('/api/chat/room/list')
+    }
+}
--- src/service/dataService.ts
+++ src/service/dataService.ts
@@ -1,11 +1,13 @@
 import { ParserAPI } from 'data/parserAPI/parserAPI'
 import { PostAPI, PostAPIInterface } from 'data/postAPI/postAPI'
 import AccountAPI, { AccountAPIInterface } from 'data/accountAPI/accountAPI'
+import ChatAPI, { ChatAPIInterface } from 'data/ChatAPI/chatAPI'
 
 export interface DataServiceInterface {
     postAPI: PostAPIInterface
     accountAPI: AccountAPIInterface
     parserAPI: ParserAPI
+    chatAPI: ChatAPIInterface
 }
 
 export class DataService implements DataServiceInterface {
@@ -15,9 +17,12 @@ export class DataService implements DataServiceInterface {
 
     parserAPI
 
+    chatAPI
+
     constructor() {
         this.postAPI = new PostAPI()
         this.accountAPI = new AccountAPI()
         this.parserAPI = new ParserAPI()
+        this.chatAPI = new ChatAPI(axiosInstance)
     }
 }
