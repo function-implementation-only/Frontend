c22d7f0fe0a96d7fd6dbb93588ac6674a45b4d31
Elio Jung <dudwns0921@gmail.com>
2022-12-28 11:19:37 +0900

    feat: redux 적용 및 카운터 예시 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
 import { Context } from './context'
 import GlobalStyle from './styles/global'
+import store from './store/store'
 
 window.context = new Context()
 // FIX ME : Vue에서는 자주 사용하는 기능을 전역 변수로 활용할 수 있는 Plugin이라는 기능이 있었는데, React에도 비슷한 방법이 있다면 바꿔주세요.
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <GlobalStyle />
-        <App />
+        <Provider store={store}>
+            <GlobalStyle />
+            <App />
+        </Provider>
     </React.StrictMode>
 )
