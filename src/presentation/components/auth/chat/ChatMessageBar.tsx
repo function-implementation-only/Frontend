8057e692c1bbfee3eefc6780f165541b4b64d124
Kyungseo-Park <troublesome.dev@gmail.com>
2022-12-30 01:53:24 +0900

    feat: 채팅 메세지 영역 구분

--- src/presentation/components/auth/chat/ChatMessageBar.tsx
+++ src/presentation/components/auth/chat/ChatMessageBar.tsx
@@ -1,3 +1,35 @@
+import styled from 'styled-components'
+
+const ChatMessageBer = styled.div`
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    width: calc(100% - 350px);
+    box-sizing: border-box;
+    padding: 100px 0rem 0rem 0rem;
+
+    .broadcaster {
+        background-color: #4a443e;
+        border-left: 2px solid #faa81a;
+        input {
+            width: calc(100% - 100px);
+        }
+        button {
+            width: 100px;
+        }
+    }
+`
+
 export default function ChatMessageBar() {
-    return <div>채팅바</div>
+    return (
+        <ChatMessageBer>
+            <div className="broadcaster">
+                {/* TODO: 작성 엑션 */}
+                <form>
+                    <input type="text" />
+                    <button type="submit">하이</button>
+                </form>
+            </div>
+        </ChatMessageBer>
+    )
 }
--- src/presentation/components/auth/chat/ChatMessageView.tsx
+++ src/presentation/components/auth/chat/ChatMessageView.tsx
@@ -1,3 +1,104 @@
+import styled from 'styled-components'
+
+const ChatMessageBox = styled.div`
+    .div {
+        position: relative;
+        width: 100%;
+        max-width: calc(100% - 6em);
+        min-width: 4em;
+        padding: 0.6em 1em 0.6em 5em;
+    }
+    .div::before {
+        content: '';
+        display: block;
+        width: 2.8em;
+        height: 2.8em;
+        position: absolute;
+        top: 0.3em;
+        left: 1em;
+        background-image: var(--profile-image);
+        background-size: 100% 100%;
+        background-position: center center;
+        background-repeat: no-repeat;
+        border-radius: 50%;
+    }
+
+    .div.deleted {
+        visibility: hidden;
+    }
+
+    .class {
+        min-width: 500px;
+        width: 80%;
+    }
+
+    .you {
+        background-color: red;
+    }
+
+    .me {
+        background-color: yellow;
+        text-align: right;
+    }
+`
+
 export default function ChatMessageView() {
-    return <div>대화</div>
+    const user = 'troublesome.dev@gmail.com'
+    const data = [
+        {
+            email: 'troublesome.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+        {
+            email: 'troublesome2.dev@gmail.com',
+            message: '하이',
+        },
+    ]
+    return (
+        <ChatMessageBox>
+            {data.map((e) =>
+                e.email === user ? (
+                    <div className="you">
+                        <span className="class">Hello</span>
+                    </div>
+                ) : (
+                    <div className="me">
+                        <span className="class">너냐</span>
+                    </div>
+                )
+            )}
+        </ChatMessageBox>
+    )
 }
