b493269985e8b98a66b323d339de7003e2c36e75
Kyungseo-Park <troublesome.dev@gmail.com>
2023-01-17 00:45:34 +0900

    feat: add token validation

--- src/data/interceptor.ts
+++ src/data/interceptor.ts
@@ -35,7 +35,11 @@ const client: ClientInstance = axios.create({
 })
 
 client.interceptors.request.use((request) => {
-    request.headers.Access_Token = localStorage.getItem('token')
+    const token = localStorage.getItem('token')
+    if (token) {
+        request.headers.Access_Token = token
+    }
+
     return request
 })
 
