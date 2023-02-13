37edd033df98f45d0c1fc9e645845010f9aefafb
powercording <adoim@naver.com>
2023-02-13 19:50:51 +0900

    Style: ChatRoom Input css적용

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,8 +1,13 @@
 import MessageItem from 'components/chat/MessageItem'
 import MessageRoom from 'components/chat/MessageRoom'
-import { MouseEvent, useState } from 'react'
+import { MouseEvent, useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import styled from 'styled-components'
+
+// import SockJs from 'sockjs-client'
+// import { over } from 'stompjs'
+
+// let stompClient = null
 
 type CategoryProps = {
     selected: boolean
@@ -13,7 +18,7 @@ const ChatPageLayout = styled.div`
     width: 1440px;
     height: calc(100vh - 80px);
     margin: 0 auto;
-    padding: 0 24px;
+    /* padding: 0 24px; */
 `
 
 const ChatListRow = styled.div`
@@ -139,6 +144,44 @@ const DUMMMY_DATA = [
     },
 ]
 
+const NO_DUMMMY_DATA = [
+    {
+        id: '1',
+        avatar: '',
+        name: '김해피',
+        content: '네~ 하겠습니다.',
+        time: '00시간',
+        email: 'te31e3123com',
+    },
+    {
+        id: '2',
+        avatar: '',
+        name: '김조인',
+        content: 'asdfagsdjdsgj.',
+        time: '00시간',
+        email: 't23123t3123c312om',
+    },
+    {
+        id: '3',
+        avatar: '',
+        name: 'Happy',
+        content: '네~ afkjsdngdsjkgdsgfddsfdsfdsfsdfdfas.',
+        time: '00시간',
+        email: 't33325244m',
+    },
+    {
+        id: '4',
+        name: '이연말',
+        avatar: '',
+        content: 'dgkhbsdgkjnsjlgknsdlkgjndjl',
+        time: '00시간',
+        email: 't32571est41com',
+    },
+]
+
+const token =
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJha3NrZmx3bjRAZ21haWwuY29tIiwiZXhwIjoxNjc2Mjg5ODE1LCJpYXQiOjE2NzYyMDM0MTV9.zJciUB2PE814L6frlBqWZD_rmba_iThLSqqtRorjZdw'
+
 function ChatPage() {
     const [AllMessage, setAllMessage] = useState(true)
     const [searchParams] = useSearchParams()
@@ -150,6 +193,43 @@ function ChatPage() {
         if (text === '전체') setAllMessage(true)
         else setAllMessage(false)
     }
+
+    // const onConnectedCallback = () => {
+    //     console.log('하이야')
+    // }
+
+    // useEffect(() => {
+    //     const Sock = new SockJs(`http://172.30.1.26/chat-service`)
+    //     stompClient = over(Sock)
+    //     stompClient.connect(
+    //         {
+    //             'Content-Type': 'application/json',
+    //             Access_Token: token,
+    //         },
+    //         onConnectedCallback
+    //     )
+    // }, [])
+
+    const getChatRooms = async () => {
+        const response = await fetch(
+            'http://172.30.1.26/chat-service/chat/list',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Access_Token: token,
+                },
+            }
+        )
+        const RoomData = await response.json()
+        // Todo: 아래리턴값 리턴 없애고 채팅방 이동으로 바꾸어야 함.
+        return RoomData
+    }
+
+    // 페이지 도착시 채팅방 리스트 갱신
+    useEffect(() => {
+        getChatRooms()
+    }, [])
 
     return (
         <ChatPageLayout>
@@ -208,7 +288,7 @@ function ChatPage() {
                     </ChatListBox>
                 </MessageRow>
             ) : (
-                <MessageRoom />
+                <MessageRoom list={NO_DUMMMY_DATA} />
             )}
         </ChatPageLayout>
     )
