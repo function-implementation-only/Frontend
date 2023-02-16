3a87bb6d6c9fb7a08f5cd6566e7ffe431d2f8d9d
Kyungseo-Park <troublesome.dev@gmail.com>
2023-02-17 02:21:16 +0900

    fix: update webstomp-client to stompjs

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig(({ mode }) => {
                 hooks: path.resolve(__dirname, './src/hooks'),
                 utils: path.resolve(__dirname, './src/utils'),
                 types: path.resolve(__dirname, './src/types'),
+                img: path.resolve(__dirname, './src/assets/images'),
             },
         },
         plugins: [
@@ -35,5 +36,17 @@ export default defineConfig(({ mode }) => {
                 },
             }),
         ],
+        server: {
+            proxy: {
+                // Proxying websockets or socket.io
+                '/chat-service/ws': {
+                    target: 'ws://61.77.108.167:8000',
+                    ws: true,
+                },
+            },
+            define: {
+                global: {},
+            },
+        },
     }
 })
